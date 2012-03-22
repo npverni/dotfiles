c30b6ce4be055b0435ef786d5494678372d0e63b
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2012-03-21 21:32:16 -0400

    abbreviated directory completion

--- oh-my-zsh/custom/plugins/jrowe/jrowe.plugin.zsh
+++ oh-my-zsh/custom/plugins/jrowe/jrowe.plugin.zsh
@@ -2,17 +2,17 @@ c() { cd "$HOME/code/$1"; }
 _c() { _files -W "$HOME/code" -/; }
 compdef _c c
 
-mhs() { cd "$HOME/code/mhs/$1"; }
-_mhs() { _files -W "$HOME/code/mhs" -/; }
-compdef _mhs mhs
+m() { cd "$HOME/code/mhs/$1"; }
+_m() { _files -W "$HOME/code/mhs" -/; }
+compdef _m m
 
 os() { cd "$HOME/code/opensource/$1"; }
 _os() { _files -W "$HOME/code/opensource" -/; }
 compdef _os os
 
-personal() { cd "$HOME/code/personal/$1"; }
-_personal() { _files -W "$HOME/code/personal" -/; }
-compdef _personal personal
+p() { cd "$HOME/code/personal/$1"; }
+_p() { _files -W "$HOME/code/personal" -/; }
+compdef _p p
 
 design() { cd "$HOME/design/$1"; }
 _design() { _files -W "$HOME/design" -/; }
